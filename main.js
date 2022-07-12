e6325510b36c96934617d00f709db64172e04691
Felipe Ma <felipeapenburg@gmail.com>
2022-07-12 00:57:35 -0300

    fix catch errors

--- main.js
+++ main.js
@@ -15,6 +15,17 @@ function getContent() {
       document.getElementById("poster").src = IMG_URL + data.poster_path
       document.getElementById("title").innerHTML = data.title
       document.getElementById("overview").innerHTML = data.overview
+      if (data.overview == "") {
+        document.getElementById("overview").innerHTML = "Sinopse não disponível"
+      }
+      if (IMG_URL + data.poster_path == "https://image.tmdb.org/t/p/original/null") {
+        document.getElementById("poster").src = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/Imagem_n%C3%A3o_dispon%C3%ADvel.svg/300px-Imagem_n%C3%A3o_dispon%C3%ADvel.svg.png"
+      }
+      if (data.success == false) {
+        document.getElementById("poster").src = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/Imagem_n%C3%A3o_dispon%C3%ADvel.svg/300px-Imagem_n%C3%A3o_dispon%C3%ADvel.svg.png"
+        document.getElementById("title").innerHTML = "Filme não encontrado"
+        document.getElementById("overview").innerHTML = "Tente novamente"
+      }
     })
 }
 
